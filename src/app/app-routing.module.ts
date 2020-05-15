1c8b45a83c4acfab58b5943b6fd6a53771f2f48b
anuwatavis <anuwat.avis@gmail.com>
2020-05-15 23:17:35 +0700

    layout setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
-import { NgModule } from '@angular/core';
+import { LoginRegisterComponent } from './pages/login-register/login-register.component';
+import { LoginRegisterLayoutComponent } from './layouts/login-register-layout/login-register-layout.component';
+import { DashboardComponent } from './pages/backend/dashboard/dashboard.component';
+import { BackendLayoutComponent } from './layouts/backend-layout/backend-layout.component';
+import { HomeComponent } from './pages/frontend/home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { FrontendLayoutComponent } from './layouts/frontend-layout/frontend-layout.component';
+const routes: Routes = [
+  //Route for call fontendLayout Page
+  {
+    path: '',
+    component: FrontendLayoutComponent,
+    children: [{ path: '', component: HomeComponent, pathMatch: 'full' }],
+  },
+  //Route for call baclEndLayout Page
+  {
+    path: 'backend',
+    component: BackendLayoutComponent,
+    children: [{ path: '', component: DashboardComponent }],
+  },
+  //Route for call LoginAndRegister Page
+  {
+    path: 'login',
+    component: LoginRegisterLayoutComponent,
+    children: [{ path: '', component: LoginRegisterComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
