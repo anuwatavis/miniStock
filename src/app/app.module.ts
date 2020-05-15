320f4b2cb380436d66c748030638956fd392880f
anuwatavis <anuwat.avis@gmail.com>
2020-05-15 16:59:39 +0700

    setup project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontendLayoutComponent } from './layouts/frontend-layout/frontend-layout.component';
+import { BackendLayoutComponent } from './layouts/backend-layout/backend-layout.component';
+import { LoginRegisterLayoutComponent } from './layouts/login-register-layout/login-register-layout.component';
+import { HomeComponent } from './pages/frontend/home/home.component';
+import { AboutComponent } from './pages/frontend/about/about.component';
+import { ContactComponent } from './pages/frontend/contact/contact.component';
+import { DashboardComponent } from './pages/backend/dashboard/dashboard.component';
+import { StockComponent } from './pages/backend/stock/stock.component';
+import { ReportComponent } from './pages/backend/report/report.component';
+import { UsersComponent } from './pages/backend/users/users.component';
+import { SettingComponent } from './pages/backend/setting/setting.component';
+import { LoginRegisterComponent } from './pages/login-register/login-register.component';
+import { HeaderComponent } from './shared/frontend/header/header.component';
+import { FooterComponent } from './shared/frontend/footer/footer.component';
+import { SidebarComponent } from './shared/backend/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontendLayoutComponent,
+    BackendLayoutComponent,
+    LoginRegisterLayoutComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    DashboardComponent,
+    StockComponent,
+    ReportComponent,
+    UsersComponent,
+    SettingComponent,
+    LoginRegisterComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
