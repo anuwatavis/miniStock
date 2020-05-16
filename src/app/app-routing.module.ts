a89d888420eaec48b085b3902664ae42278cb355
anuwatavis <anuwat.avis@gmail.com>
2020-05-16 09:43:54 +0700

    add login register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ContactComponent } from './pages/frontend/contact/contact.component';
 import { LoginRegisterComponent } from './pages/login-register/login-register.component';
 import { LoginRegisterLayoutComponent } from './layouts/login-register-layout/login-register-layout.component';
 import { DashboardComponent } from './pages/backend/dashboard/dashboard.component';
@@ -6,12 +7,20 @@ import { HomeComponent } from './pages/frontend/home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FrontendLayoutComponent } from './layouts/frontend-layout/frontend-layout.component';
+import { AboutComponent } from './pages/frontend/about/about.component';
 const routes: Routes = [
   //Route for call fontendLayout Page
   {
     path: '',
     component: FrontendLayoutComponent,
-    children: [{ path: '', component: HomeComponent, pathMatch: 'full' }],
+    children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      { path: 'contact', component: ContactComponent },
+    ],
   },
   //Route for call baclEndLayout Page
   {
